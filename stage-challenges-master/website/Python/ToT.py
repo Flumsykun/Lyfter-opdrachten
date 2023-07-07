d39199187a00443f9213f48d3c44019a313fe528
Flumsykun <jgjvanderee@gmail.com>
2023-07-07 02:08:18 +0200

    verbetering

--- stage-challenges-master/website/Python/ToT.py
+++ stage-challenges-master/website/Python/ToT.py
@@ -1,24 +1,14 @@
-def distribute_candy(vampires, zombies, witches, houses):
-    total_candies = vampires * 3 + zombies * 4 + witches * 5
-    candies_per_child = total_candies // houses
-    return candies_per_child
-
 # Read input from input.txt
 with open('./Input/input2.txt', 'r') as file:
     input_data = file.readlines()
 
 # Process each line and calculate candies per child
-output_data = []
-for line in input_data:
-    group = line.strip().split(', ')
-    vampires = int(group[0].split(': ')[1])
-    zombies = int(group[1].split(': ')[1])
-    witches = int(group[2].split(': ')[1])
-    houses = int(group[3].split(': ')[1])
-    candies_per_child = distribute_candy(vampires, zombies, witches, houses)
-    output_data.append(str(candies_per_child))
+output_data = [
+    str(sum(int(x.split(': ')[1]) * y for x, y in zip(group, [3, 4, 5])) // int(group[3].split(': ')[1]))
+    for group in (line.strip().split(', ') for line in input_data)
+]
 
-# Write output to output.html
+# Write output to output3.html
 with open('output3.html', 'w') as file:
     file.write('<html>\n')
     file.write('<head>\n')
@@ -32,3 +22,4 @@ with open('output3.html', 'w') as file:
     file.write('  </ul>\n')
     file.write('</body>\n')
     file.write('</html>')
+
